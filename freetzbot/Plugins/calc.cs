5c2b5867fa6f61f0fa93a83ac29733da82be8d0e
Suchiman <robinsue@live.de>
2013-10-07 00:48:44 +0200

    calc: - Unterstützung für hochgestellte Zahlen - Weitere Fehlerszenarien abgefangen

--- freetzbot/Plugins/calc.cs
+++ freetzbot/Plugins/calc.cs
@@ -1,6 +1,11 @@
 ﻿using FritzBot.DataModel;
+using HtmlAgilityPack;
 using Newtonsoft.Json;
 using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace FritzBot.Plugins
 {
@@ -9,19 +14,64 @@ namespace FritzBot.Plugins
     [Module.ParameterRequired]
     class calc : PluginBase, ICommand
     {
+        char[] superscripts = { '⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹' };
+
         public void Run(ircMessage theMessage)
         {
             string result = toolbox.GetWeb(String.Format("http://www.google.com/ig/calculator?q={0}", toolbox.UrlEncode(theMessage.CommandLine)));
+            result = OktalUnescape(result);
 
             CalculationResult cr = JsonConvert.DeserializeObject<CalculationResult>(result);
-            if (String.IsNullOrEmpty(cr.error))
+            if (cr == null)
             {
-                theMessage.Answer(String.Format("{0} ergibt {1}", cr.lhs, cr.rhs));
+                theMessage.Answer("Fehler beim Aufruf der API");
+            }
+            else if (!String.IsNullOrEmpty(cr.error))
+            {
+                theMessage.Answer("Die Eingabe ist ungültig oder konnte nicht interpretiert werden: " + cr.error);
             }
             else
             {
-                theMessage.Answer("Die Eingabe ist ungültig oder konnte nicht interpretiert werden");
+                theMessage.Answer(String.Format("{0} ergibt {1}", FixOutput(cr.lhs), FixOutput(cr.rhs)));
             }
+        }
+
+        public string FixOutput(string input)
+        {
+            input = HtmlEntity.DeEntitize(input);
+            input = Regex.Replace(input, @"\<sup\>(\d*)\<\/sup\>", m => new String(m.Groups[1].Value.ToCharArray().Select(x => superscripts[Convert.ToInt32(x.ToString())]).ToArray()));
+            return input;
+        }
+
+        public string OktalUnescape(string input)
+        {
+            StringBuilder _buffer = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                if (currentChar == '\\' && input[i + 1] == 'x')
+                {
+                    currentChar = input[++i];
+                }
+                else
+                {
+                    _buffer.Append(currentChar);
+                    continue;
+                }
+
+                char[] hexValues = new char[2];
+                char hexChar;
+                for (int h = 0; h < hexValues.Length; h++)
+                {
+                    if (i + 1 < input.Length && (currentChar = input[++i]) != '\0')
+                        hexValues[h] = currentChar;
+                }
+
+                hexChar = Convert.ToChar(int.Parse(new string(hexValues), NumberStyles.HexNumber, NumberFormatInfo.InvariantInfo));
+                _buffer.Append(hexChar);
+            }
+            return _buffer.ToString();
         }
     }
 
