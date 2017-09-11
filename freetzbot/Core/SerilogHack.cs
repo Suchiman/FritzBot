02cc9b4600cf42a28129e1b05944b24c22634491
Suchiman <robinsue@live.de>
2017-09-11 17:58:14 +0200

    SerilogHack: Hack no longer necessary

--- freetzbot/Core/SerilogHack.cs
+++ freetzbot/Core/SerilogHack.cs
@@ -1,29 +1,16 @@
 ﻿using Serilog;
 using Serilog.Events;
 using System;
-using System.Collections.Generic;
 
 namespace FritzBot.Core
 {
     static class SerilogHack
     {
-        delegate void ProcessDelegate(string messageTemplate, object[] messageTemplateParameters, out MessageTemplate parsedTemplate, out IEnumerable<LogEventProperty> properties);
-
-        private static ProcessDelegate ProcessMethod;
-
-        static SerilogHack()
-        {
-            var _messageTemplateProcessor = Log.Logger.GetType().GetField("_messageTemplateProcessor", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-            ProcessMethod = (ProcessDelegate)_messageTemplateProcessor.FieldType.GetMethod("Process").CreateDelegate(typeof(ProcessDelegate), _messageTemplateProcessor.GetValue(Log.Logger));
-        }
-
         public static LogEvent CreateLogEvent(DateTimeOffset timestamp, LogEventLevel level, Exception exception, string messageTemplate, params object[] messageTemplateParameters)
         {
-            MessageTemplate parsedTemplate;
-            IEnumerable<LogEventProperty> properties;
-            ProcessMethod(messageTemplate, messageTemplateParameters, out parsedTemplate, out properties);
+            Log.BindMessageTemplate(messageTemplate, messageTemplateParameters, out var parsedTemplate, out var boundProperties);
 
-            return new LogEvent(timestamp, level, exception, parsedTemplate, properties);
+            return new LogEvent(timestamp, level, exception, parsedTemplate, boundProperties);
         }
     }
 }
