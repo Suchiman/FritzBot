f5c7593f5e707875370956302d9225d208304844
Suchiman <robinsue@live.de>
2018-09-19 21:59:13 +0200

    Modernize mem plugin

--- freetzbot/Plugins/mem.cs
+++ freetzbot/Plugins/mem.cs
@@ -1,9 +1,8 @@
 using FritzBot.DataModel;
-using Serilog;
 using System;
 using System.Diagnostics;
-using System.Reflection;
 using System.Runtime;
+using System.Runtime.InteropServices;
 
 namespace FritzBot.Plugins
 {
@@ -14,35 +13,9 @@ namespace FritzBot.Plugins
     {
         public void Run(IrcMessage theMessage)
         {
-            string version = Environment.Version.ToString(2);
-            string os = Environment.OSVersion.ToString();
-            Type t = Type.GetType("Mono.Runtime");
-            if (t != null)
-            {
-                MethodInfo displayName = t.GetMethod("GetDisplayName", BindingFlags.NonPublic | BindingFlags.Static);
-                if (displayName != null)
-                {
-                    version = (string)displayName.Invoke(null, null);
-                    version = "Mono " + version.Substring(0, version.IndexOf('('));
-                }
-                try
-                {
-                    ProcessStartInfo pi = new ProcessStartInfo("uname", "-s -r -m");
-                    pi.RedirectStandardOutput = true;
-                    pi.UseShellExecute = false;
-                    Process p = new Process();
-                    p.StartInfo = pi;
-                    p.Start();
-                    os = p.StandardOutput.ReadLine();
-                }
-                catch (Exception ex)
-                {
-                    Log.Error(ex, "Abrufen der Kernelversion fehlgeschlagen");
-                }
-            }
             GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
             GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true, true);
-            theMessage.Answer($"Betriebssystem: {os}, RuntimeVersion: {version.Trim()} {IntPtr.Size * 8}bit, CPU's: {Environment.ProcessorCount}, RAM Verbrauch (Programm / +Runtime): {ToMB(GC.GetTotalMemory(true))}MB / {ToMB(Process.GetCurrentProcess().WorkingSet64)}MB");
+            theMessage.Answer($"Betriebssystem: {RuntimeInformation.OSDescription}, RuntimeVersion: {RuntimeInformation.FrameworkDescription} {IntPtr.Size * 8}bit, CPU's: {Environment.ProcessorCount}, RAM Verbrauch (Programm / +Runtime): {ToMB(GC.GetTotalMemory(true))}MB / {ToMB(Process.GetCurrentProcess().WorkingSet64)}MB");
         }
 
         private string ToMB(long value)
